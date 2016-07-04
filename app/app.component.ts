45d6f303ce4e7f545982c1075e2b61ecafe0cf24
fscbest <romangeyber@gmail.com>
2016-07-04 16:33:27 +0300

    Configuring Routes

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,14 @@
 import {Component} from 'angular2/core';
+import {RouteConfig} from 'angular2/router';
 
+import {AlbumComponent} from "./album.component";
+import {ContactComponent} from "./contact.component";
+
+@RouteConfig([
+    { path: '/albums', name: 'Albums', component: AlbumComponent, useAsDefault: true },
+    { path: '/contact', name: 'Contact', component: ContactComponent },
+    { path: '/*other', name: 'Other', redirectTo: ['Albums'] }
+])
 @Component({
     selector: 'my-app',
     templateUrl: '/app/app.component.html'
