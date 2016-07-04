6e19e3091eecd85a43744f41fb81546262135290
fscbest <romangeyber@gmail.com>
2016-07-04 16:43:48 +0300

    Router Outlet

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,17 +1,18 @@
 import {Component} from 'angular2/core';
-import {RouteConfig} from 'angular2/router';
+import {RouteConfig, RouterOutlet} from 'angular2/router';
 
-import {AlbumComponent} from "./album.component";
 import {ContactComponent} from "./contact.component";
+import {AlbumsComponent} from "./albums.component";
 
 @RouteConfig([
-    { path: '/albums', name: 'Albums', component: AlbumComponent, useAsDefault: true },
+    { path: '/albums', name: 'Albums', component: AlbumsComponent, useAsDefault: true },
     { path: '/contact', name: 'Contact', component: ContactComponent },
     { path: '/*other', name: 'Other', redirectTo: ['Albums'] }
 ])
 @Component({
     selector: 'my-app',
-    templateUrl: '/app/app.component.html'
+    templateUrl: '/app/app.component.html',
+    directives: [RouterOutlet]
 })
 export class AppComponent {
 }
